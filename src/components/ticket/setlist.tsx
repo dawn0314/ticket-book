df980324ad0d14b55e89da1899c046bde9edf7bd
dawn0314 <sdawn0314@gmail.com>
2024-03-29 01:07:52 +0900

    style: Add add track button

--- src/components/ticket/setlist.tsx
+++ src/components/ticket/setlist.tsx
@@ -1,15 +1,31 @@
-import styled from "styled-components";
+import { styled, css } from "styled-components";
 import MusicSearch from "./music-search";
+import LibraryMusicRoundedIcon from "@mui/icons-material/LibraryMusicRounded";
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: #ddd;
+  background-color: var(--light);
+`;
+
+const AddTrack = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  border-radius: 20px;
+  padding: 6px;
+  font-weight: 600;
+  color: #fff;
+  background-color: var(--primary-dark);
+  border: none;
 `;
 export default function Setlist() {
   return (
     <Wrapper>
-      <MusicSearch />
+      <AddTrack>
+        <LibraryMusicRoundedIcon />
+        Add Track
+      </AddTrack>
     </Wrapper>
   );
 }
