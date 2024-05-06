d5e5abab354b0826b9fbca4c246a35f3bbee7b97
dawn0314 <sdawn0314@gmail.com>
2024-05-07 01:00:51 +0900

    refactor: edit pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ArrowBackIosRoundedIcon from "@mui/icons-material/ArrowBackIosRounded";
-import KeyboardDoubleArrowLeftRoundedIcon from "@mui/icons-material/KeyboardDoubleArrowLeftRounded";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
-import KeyboardDoubleArrowRightRoundedIcon from "@mui/icons-material/KeyboardDoubleArrowRightRounded";
 
 export default function Pagination({ totalPage, limit, page, setPage }) {
   const [currentPageArray, setCurrentPageArray] = useState([]);
   const [totalPageArray, setTotalPageArray] = useState([]);
 
   useEffect(() => {
-    if (page % limit === 1) {
-      setCurrentPageArray(totalPageArray[Math.floor(page / limit)]);
-    } else if (page % limit === 0) {
-      setCurrentPageArray(totalPageArray[Math.floor(page / limit) - 1]);
-    }
+    const currentPageIndex = Math.floor((page - 1) / limit);
+    setCurrentPageArray(totalPageArray[currentPageIndex]);
   }, [page]);
 
   useEffect(() => {
@@ -24,24 +19,20 @@ export default function Pagination({ totalPage, limit, page, setPage }) {
   }, [totalPage]);
 
   const sliceArrayByLimit = (totalPage, limit) => {
-    const totalPageArray = Array(totalPage)
-      .fill()
-      .map((_, i) => i);
-    return Array(Math.ceil(totalPage / limit))
-      .fill()
-      .map(() => totalPageArray.splice(0, limit));
+    const totalPageArray = [...Array(totalPage).keys()];
+    return Array.from({ length: Math.ceil(totalPage / limit) }, (_, i) =>
+      totalPageArray.slice(i * limit, (i + 1) * limit)
+    );
   };
 
   return (
     <Wrapper>
-      <ArrowBackIosRoundedIcon
-        onClick={() => setPage(page - 1)}
-        disabled={page === 1}
-      />
+      <LeftPageButton onClick={() => setPage(page - 1)} disabled={page === 1} />
       <ButtonWrapper>
         {currentPageArray?.map((i) => (
           <PageButton
             key={i + 1}
+            className={page === i + 1 && "active"}
             onClick={() => setPage(i + 1)}
             aria-current={page === i + 1 ? "page" : null}
           >
@@ -49,7 +40,7 @@ export default function Pagination({ totalPage, limit, page, setPage }) {
           </PageButton>
         ))}
       </ButtonWrapper>
-      <ArrowForwardIosRoundedIcon
+      <RightPageButton
         onClick={() => setPage(page + 1)}
         disabled={page === totalPage}
       />
@@ -65,10 +56,37 @@ const Wrapper = styled.div`
 
 const ButtonWrapper = styled.div`
   display: flex;
-  margin: 0 12px;
-  width: 100px;
+  width: 150px;
   justify-content: space-around;
 `;
+
 const PageButton = styled.div`
   cursor: pointer;
+  text-align: center;
+  border-radius: 50%;
+  padding-top: 7px;
+  width: 30px;
+  height: 30px;
+
+  &:hover {
+    border: solid 1px var(--primary-dark);
+  }
+  &.active {
+    width: 30px;
+    height: 30px;
+    background-color: var(--accent);
+    color: white;
+    font-weight: 700;
+    border: 0;
+  }
+`;
+
+const LeftPageButton = styled(ArrowBackIosRoundedIcon)`
+  cursor: pointer;
+  pointer-events: ${(props) => (props.disabled ? "none" : "auto")};
+`;
+
+const RightPageButton = styled(ArrowForwardIosRoundedIcon)`
+  cursor: pointer;
+  pointer-events: ${(props) => (props.disabled ? "none" : "auto")};
 `;
