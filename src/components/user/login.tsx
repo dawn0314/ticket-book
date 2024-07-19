79ebffcba8741c75b0355cd4e7e3091a8b9b8976
dawn0314 <sdawn0314@gmail.com>
2024-07-19 11:31:42 +0900

    Add login page

---
+++ src/components/user/login.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { auth } from "../../firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    if (isLoading || email === "" || password === "") return;
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Log into Ticket Book</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          value={password}
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account?{" "}
+        <Link to="/create-account">Create one &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  color: white;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
+const Switcher = styled.span`
+  margin-top: 20px;
+  color: white;
+  a {
+    color: #1d9bf0;
+  }
+`;
