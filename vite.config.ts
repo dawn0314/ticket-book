5a938d3932af7b493a7ec64d5ca74f7c0ac94873
dawn0314 <sdawn0314@gmail.com>
2024-12-02 16:27:03 +0900

    change config base

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === "production" ? "/ticket-book/" : "/", // 배포 환경에서만 base 설정
-  };
+export default defineConfig({
+  plugins: [react()],
 });
